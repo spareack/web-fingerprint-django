fa5cf8fe3e66b1102900218fe97a9db74fdc3789
ASUS <anonimus.pochti@gmail.com>
2022-04-22 21:21:59 +0300

    ip_location

--- main/views.py
+++ main/views.py
@@ -10,12 +10,25 @@ def get_location_data(ip_address):
     response = requests.get(f'https://ipapi.co/{ip_address}/json/').json()
     return response
 
+
+def get_ip_address(data):
+    headers_list = 'HTTP_CLIENT_IP', 'HTTP_X_FORWARDED_FOR', 'HTTP_X_FORWARDED', 'HTTP_X_CLUSTER_CLIENT_IP', \
+                   'HTTP_FORWARDED_FOR', 'HTTP_FORWARDED', 'REMOTE_ADDR'
+
+    for header in headers_list:
+        if header in data:
+            return data.get(header)
+
+
+
 class HomeView(View):
     template_name = 'main/index.html'
 
     def get(self, request):
         params = {key: request.META.get(key) for key in request.META if key != 'wsgi.file_wrapper'}
-        location_data = get_location_data(request.META.get('REMOTE_ADDR'))
+
+        ip_address = get_ip_address(params)
+        location_data = get_location_data(request.META.get(ip_address))
 
         context = {'params': params, 'location_data': location_data}
 
