a653c27ba0a764aaaf845e12c59f5d18230c9c17
ASUS <anonimus.pochti@gmail.com>
2022-04-23 02:09:13 +0300

    filesendfix

--- main/views.py
+++ main/views.py
@@ -1,4 +1,6 @@
 
+from user_agents import parse
+
 from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponse
@@ -12,24 +14,36 @@ def get_location_data(ip_address):
 
 
 def get_ip_address(data):
-    headers_list = 'HTTP_CLIENT_IP', 'HTTP_X_REAL_IP', 'HTTP_X_FORWARDED_FOR', 'HTTP_X_FORWARDED', \
-                   'HTTP_X_CLUSTER_CLIENT_IP', 'HTTP_FORWARDED_FOR', 'HTTP_FORWARDED', 'REMOTE_ADDR'
+    headers_list = ['HTTP_X_REAL_IP', 'HTTP_CLIENT_IP', 'HTTP_X_FORWARDED_FOR', 'HTTP_X_FORWARDED',
+                    'HTTP_X_CLUSTER_CLIENT_IP', 'HTTP_FORWARDED_FOR', 'HTTP_FORWARDED', 'REMOTE_ADDR']
 
     for header in headers_list:
         if header in data:
             return data.get(header)
 
 
+def parse_user_agent(user_agent):
+    user_agent_info = parse(user_agent)
+    return str(user_agent_info)
+
+
 class HomeView(View):
     template_name = 'main/index.html'
 
     def get(self, request):
-        params = {key: request.META.get(key) for key in request.META if key != 'wsgi.file_wrapper'}
+        params = {key: request.META.get(key) for key in request.META if not key.startwith('wsgi.')}
 
         ip_address = get_ip_address(request.META)
         location_data = get_location_data(ip_address)
 
-        context = {'params': params, 'location_data': location_data, 'ip_timezone': location_data['utc_offset']}
+        location_keys_list = ['ip', 'city', 'country_code', 'country_name', 'timezone', 'languages']
+
+        user_agent_info = parse_user_agent(request.META.get('HTTP_USER_AGENT'))
+
+        context = {'params': params,
+                   'location_data': location_data,
+                   'ip_timezone': location_data.get('utc_offset'),
+                   'user_agent_info': user_agent_info}
 
         # print(request.headers)
 
